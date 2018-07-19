7fa2d436be26a9155132a2de8d4e1e913d6844fd
mandersen21 <mikkelandersen21@gmail.com>
2018-07-19 19:52:23 +0200

    Added crud changes.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const express = require('express')
+const express = require('express');
+const Joi = require('joi');
+
 const app = express();
+
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'Course1' },
@@ -17,9 +22,51 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Course was not found')
+    if (!course) return res.status(404).send('Course was not found')
     res.send(course)
 })
+
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body); // getting result.error
+    if (error) return res.status(400).send(result.error);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Course with the given ID was not found');
+
+    // const result = validateCourse(req.body);
+    const { error } = validateCourse(req.body); // getting result.error
+    if (error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Course with the given ID was not found');
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1)
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listeling on port ' + port))
